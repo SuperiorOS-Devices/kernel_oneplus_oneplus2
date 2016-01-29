e15684d5d3b797bebe21184cecadcb7c866cbe22
andip71 <andreasp@gmx.de>
2016-01-29 14:38:08 +0100

    cpufreq: ondemand: add support for saving tunables per policy governor instance
    credits to ZaneZam
    
    relevant parts repectfully borrowed from elementalx governor:
    https://github.com/zanezam/boeffla-kernel-oos-oneplus2/blob/boeffla/drivers/cpufreq/cpufreq_elementalx.c

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -30,6 +30,8 @@
 static DEFINE_PER_CPU(struct od_cpu_dbs_info_s, od_cpu_dbs_info);
 static DEFINE_PER_CPU(struct od_dbs_tuners *, cached_tuners);
 
+static DEFINE_PER_CPU(struct od_dbs_tuners *, cached_tuners);
+
 static struct od_ops od_ops;
 
 #ifndef CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND
@@ -476,30 +478,63 @@ static struct attribute_group od_attr_group_gov_pol = {
 /************************** sysfs end ************************/
 
 static void save_tuners(struct cpufreq_policy *policy,
-			  struct od_dbs_tuners *tuners)
+		struct od_dbs_tuners *tuners)
 {
 	int cpu;
 
 	if (have_governor_per_policy())
-		cpu = cpumask_first(policy->related_cpus);
+	    cpu = cpumask_first(policy->related_cpus);
 	else
-		cpu = 0;
+	    cpu = 0;
 
 	WARN_ON(per_cpu(cached_tuners, cpu) &&
-		per_cpu(cached_tuners, cpu) != tuners);
+	per_cpu(cached_tuners, cpu) != tuners);
 	per_cpu(cached_tuners, cpu) = tuners;
 }
 
 static struct od_dbs_tuners *alloc_tuners(struct cpufreq_policy *policy)
 {
 	struct od_dbs_tuners *tuners;
-	u64 idle_time;
-	int cpu;
 
 	tuners = kzalloc(sizeof(*tuners), GFP_KERNEL);
 	if (!tuners) {
-		pr_err("%s: kzalloc failed\n", __func__);
-		return ERR_PTR(-ENOMEM);
+	    pr_err("%s: kzalloc failed\n", __func__);
+	    return ERR_PTR(-ENOMEM);
+	}
+
+	tuners->sampling_down_factor = DEF_SAMPLING_DOWN_FACTOR;
+	tuners->ignore_nice_load = 0;
+	tuners->powersave_bias = default_powersave_bias;
+	tuners->io_is_busy = should_io_be_busy();
+
+	save_tuners(policy, tuners);
+
+	return tuners;
+}
+
+static struct od_dbs_tuners *restore_tuners(struct cpufreq_policy *policy)
+{
+	int cpu;
+
+	if (have_governor_per_policy())
+	    cpu = cpumask_first(policy->related_cpus);
+	else
+	    cpu = 0;
+
+	return per_cpu(cached_tuners, cpu);
+}
+
+static int od_init(struct dbs_data *dbs_data, struct cpufreq_policy *policy)
+{
+	struct od_dbs_tuners *tuners;
+	u64 idle_time;
+	int cpu;
+
+	tuners = restore_tuners(policy);
+	if (!tuners) {
+		tuners = alloc_tuners(policy);
+		if (IS_ERR(tuners))
+			return PTR_ERR(tuners);
 	}
 
 	cpu = get_cpu();
@@ -570,7 +605,7 @@ static int od_init(struct dbs_data *dbs_data, struct cpufreq_policy *policy)
 
 static void od_exit(struct dbs_data *dbs_data)
 {
-	//nothing to do
+//	nothing to do
 }
 
 define_get_cpu_dbs_routines(od_cpu_dbs_info);
@@ -590,7 +625,7 @@ static struct common_dbs_data od_dbs_cdata = {
 	.gov_dbs_timer = od_dbs_timer,
 	.gov_check_cpu = od_check_cpu,
 	.gov_ops = &od_ops,
-	.init = od_init,
+	.init_od = od_init,
 	.exit = od_exit,
 };
 
@@ -668,6 +703,7 @@ static void __exit cpufreq_gov_dbs_exit(void)
 	int cpu;
 
 	cpufreq_unregister_governor(&cpufreq_gov_ondemand);
+
 	for_each_possible_cpu(cpu) {
 		kfree(per_cpu(cached_tuners, cpu));
 		per_cpu(cached_tuners, cpu) = NULL;
