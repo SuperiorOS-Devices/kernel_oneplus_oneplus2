dc00a3629841b740a8ffe0d04a4c5975c8a1ddb0
abhijiths0098 <abhivarma362@gmail.com>
2019-04-02 11:30:04 +0530

    correct format for compiling

--- scripts.sh
+++ scripts.sh
@@ -1,32 +1,33 @@
 ###Matrix Building Scripts###
 
-AK_DIR="/home/abhijith/dev/anykernel3"
-ZIP_MOVE="/home/abhijith/dev/ZIPS"
-KROOT="$(pwd)"
-IMAGE_DIR="$KROOT/arch/arm64/boot"
-REL="aosp"
-VER="1.6"
+AK_DIR="/home/abhijith/dev/anykernel3" #         change         #
+ZIP_MOVE="/home/abhijith/dev/ZIPS"     #       accrodingly      #
+KROOT="$(pwd)"                         #           to           #
+IMAGE_DIR="$KROOT/arch/arm64/boot"     #          your          #
+REL="1.6"                              #       directories      #
+VER="aosp"                             #    Have a nice Day!!   #
 
 
-#STEP-1
+#STEP-1-Always build clean
 chmod +x "scripts.sh"
 make "clean"
 
-#STEP-2
+#STEP-2-Compiling the kernel
 TOOLCHAIN="/home/abhijith/dev/toolchain/bin/aarch64-linux-android-"
 export CROSS_COMPILE="/home/abhijith/dev/toolchain/bin/aarch64-linux-android-"
 ARCHITECTURE=arm64
 
-#STEP-4
+#STEP-3-creating config for kernel
 make "matrix_defconfig"
 
-#STEP-3
+#STEP-4-menuconfig
 make "menuconfig"
 
-#STEP-5
-make "-j4"
 
-#STEP-6
+#STEP-5-Start building!!!!!
+make "-j4"       # -j (change the number according to you pc threads)
+
+#STEP-6(credits to frap129)
 
                 cp -vr $IMAGE_DIR/Image.gz-dtb $AK_DIR/
 
