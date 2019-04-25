e212b85c9e94bcbd04c9ab70c523c5f0d860ebe9
abhijiths0098 <abhijiths0098@gmail.com>
2019-04-25 04:47:55 +0000

    Revert "compile errors"
    This reverts commit 69222a5ef1dc8c05c1d5acc88a09205304443223.

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -327,6 +327,7 @@ int suspend_devices_and_enter(suspend_state_t state)
  */
 static void suspend_finish(void)
 {
+	msm_do_pm_boost(true);
 	suspend_thaw_processes();
 	pm_notifier_call_chain(PM_POST_SUSPEND);
 	pm_restore_console();
